90c852b05faef3663cbc54a8b429e4ad4e167163
akhilchakali <akhia1686@gmail.com>
2024-10-20 19:44:19 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { createContext,  useState} from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Header from './header'
+import Home from './home'
+import About from './about'
+export const NavContext = createContext(null)
+
+const App = () => {
+    const [theme,setTheme] = useState('light')
+    const backgroundColor = theme === 'light' ? '#f0f0f0' : '#333';
+    return (
+        <NavContext.Provider value={{theme , setTheme}}>
+            <div style={{ backgroundColor: backgroundColor}}>
+            <BrowserRouter >
+            <Header/>
+            <Routes>
+                <Route exact path='/' element={<Home/>}/>
+                <Route exact path='/about' element={<About/>}/>
+            </Routes>
+            </BrowserRouter>
+            </div>
+        </NavContext.Provider>
+    )
 }
 
-export default App;
+export default App
