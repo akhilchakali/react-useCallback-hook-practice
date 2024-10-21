668f4aff576325b4a6a851f41753221b0b7b78d3
akhilchakali <akhia1686@gmail.com>
2024-10-21 09:20:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,18 @@
-import { createContext,  useState} from 'react'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import {Routes, Route, BrowserRouter} from 'react-router-dom'
 
-import Header from './header'
+import Login from './login'
 import Home from './home'
-import About from './about'
-export const NavContext = createContext(null)
 
 const App = () => {
-    const [theme,setTheme] = useState('light')
-    const backgroundColor = theme === 'light' ? '#f0f0f0' : '#333';
-    return (
-        <NavContext.Provider value={{theme , setTheme}}>
-            <div style={{ backgroundColor: backgroundColor}}>
-            <BrowserRouter >
-            <Header/>
-            <Routes>
-                <Route exact path='/' element={<Home/>}/>
-                <Route exact path='/about' element={<About/>}/>
-            </Routes>
-            </BrowserRouter>
-            </div>
-        </NavContext.Provider>
+    return(
+        <div>
+        <BrowserRouter>
+        <Routes>
+            <Route exact path='/' element={<Login/>}/>
+            <Route exact path="/home" element={<Home/>}/>
+        </Routes>
+        </BrowserRouter>
+        </div> 
     )
 }
-
 export default App
--- src/home.js
+++ src/home.js
@@ -1,7 +1,14 @@
+import { useNavigate } from "react-router-dom"
+
 const Home = () => {
+    const navigate = useNavigate()
+    const Logout = () => {
+        navigate('/')
+    }
     return(
-        <h1>Home</h1>
+        <div>
+            <button onClick={Logout}>Logout</button>
+        </div>
     )
 }
-
 export default Home
